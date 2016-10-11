d9486d1a2c2e1e19ec6a4a51c9c5f749417799ef
root <ichduschenackt@gmail.com>
2016-10-11 14:16:03 +0200

    The archiver now correctly ignores the .gitignore file creating zip archives. This fix is needed for: https://jira.neos.io/browse/NEOS-1885

--- src/Composer/Package/Archiver/GitExcludeFilter.php
+++ src/Composer/Package/Archiver/GitExcludeFilter.php
@@ -30,12 +30,6 @@ class GitExcludeFilter extends BaseExcludeFilter
     {
         parent::__construct($sourcePath);
 
-        if (file_exists($sourcePath.'/.gitignore')) {
-            $this->excludePatterns = $this->parseLines(
-                file($sourcePath.'/.gitignore'),
-                array($this, 'parseGitIgnoreLine')
-            );
-        }
         if (file_exists($sourcePath.'/.gitattributes')) {
             $this->excludePatterns = array_merge(
                 $this->excludePatterns,
